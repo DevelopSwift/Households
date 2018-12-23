e8b3524d90390def9aac62bb851c04d987121df3
DevelopSwift <DevelopSwift@yahoo.com>
2018-12-23 14:54:52 +0100

    Initial Commit

---
+++ Households/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Households
+//
+//  Created by Fabian Grochowalski on 23.12.18.
+//  Copyright © 2018 DevelopSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
